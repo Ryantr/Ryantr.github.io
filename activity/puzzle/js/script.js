c7666bd0ef428bc316e3f73681c515c815eca0eb
tianran <tianran@mafengwo.com>
2017-06-23 20:00:37 +0800

    fix

--- activity/puzzle/js/script.js
+++ activity/puzzle/js/script.js
@@ -171,7 +171,7 @@ puzzleGame.prototype = {
                             self.thisLeft = $(this).css("left");
                             self.thisIndex = Math.floor(parseInt(self.thisTop)/self.cellHeight)*self.cellCol;
                             self.thisIndex += Math.floor(parseInt(self.thisLeft)/self.cellWidth);
-                            alert(self.thisIndex);
+                            alert(parseInt(self.thisLeft)/self.cellWidth);
                         } else if($(".selected").length == 2){
                             self.clickBusy = true;
 
